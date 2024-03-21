9732459c4887912a149931c31edc6c630b437f36
Francisco Arellano <68157946+SilverDaml@users.noreply.github.com>
2024-03-21 22:52:47 +0000

    First commit

---
+++ src/modules/todoCreator.js
@@ -0,0 +1,10 @@
+class TodoCreator {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = new Date(dueDate);
+        this.priority = priority;
+    }
+
+     todo = document.createElement(div);
+}
